40293d1406dad037573df0e9bd092fec77a93297
Shahan Neda <shahan.neda@gmail.com>
2020-04-07 15:36:11 -0700

    setup socketio

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express")();
 
-app.listen(port, function(){
+const server = require("http").Server(express); // create the http servere and conenct it to socket io
 
-        console.log("NedaChat Server Started!");
+const io = require("socket.io")(server); // setup socekt io
 
+server.listen(80);
+console.log("NedaChat Server Started!");
+
+
+app.get('/', function (req, res) {
+        //res.sendFile(__dirname + '/index.html');
+        io.on('connection', function (socket) {
+                
+
+                socket.emit('news', { 
+                        hello: 'world' 
+                });
+
+                socket.on('my other event', function (data) {
+                        console.log(data);
+                });
+
+                socket.on("connect", function(data){
+                        console.log("New User Connected: name" + data.user.name);
+                        io.emit("newUser", data);
+                }
+
+
+        });
 });
+
