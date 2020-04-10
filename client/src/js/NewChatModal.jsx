6fe0f49d160e3a2802d227d388b6bcad5c3c9481
Shahan Neda <shahan.neda@gmail.com>
2020-04-10 16:54:31 -0700

    added add new chat modal

--- client/src/js/NewChatModal.jsx
+++ client/src/js/NewChatModal.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import Message from "./Message.jsx"
+
 class NewChatModal extends Component{
 
         constructor(props){
@@ -7,21 +7,54 @@ class NewChatModal extends Component{
 
                 this.state = {
                 };
+
+        }
+
+        componentDidMount() {
+                $(this.modal).modal('show');
+                $(this.modal).on('hidden.bs.modal', this.props.close);
+        }
+        componentDidUpdate(){
         }
 
         render(){
                 return(
-                        <div className="card col-10">
-                                New Chat Modal
+                        <div className="modal " tabIndex="1" role="dialog" ref={modal=> this.modal = modal}>
+
+                                <div className="modal-dialog" role="document">
+                                        <div className="modal-content">
+                                                <div className="modal-header">
+                                                        <h5 className="modal-title">New Chat</h5>
+                                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                                <span>&times;</span>
+                                                        </button>
+                                                </div>
+
+                                                <div className="modal-body">
+                                                        <form>
+                                                                <div className="form-group">
+                                                                        <label htmlFor="users-in-chat" className="col-form-label"> Users to add in chat: </label> 
+                                                                        <input type="text" className="form-control" id="users-in-chat" />
+                                                                </div>
+
+                                                                <div className="form-group">
+                                                                        <label htmlFor="chat-name" className="col-form-label">Chat name</label> 
+                                                                        <input type="text" className="form-control" id="chat-name" />
+                                                                </div>
+
+                                                        </form>
+                                                        <div className="modal-footer">
+                                                                <button type="button" className="btn btn-primary">Create Chat</button>
+                                                        </div>
+                                                </div>                                                
+                                        </div>
+                                </div>
+
                         </div>
                 );
 
         }
-        componentDidMount() {
-        }
 
-        componentDidUpdate() {
-        }
 
 }
 
