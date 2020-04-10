662f988752ada6532b5730334488b98c62a8a103
Shahan Neda <shahan.neda@gmail.com>
2020-04-09 17:44:29 -0700

    added send message on enter

--- client/src/js/MessageSend.jsx
+++ client/src/js/MessageSend.jsx
@@ -25,16 +25,22 @@ class MessageSend extends Component{
 
                 }); 
         }
+        keyPress = (e) => {
+                if(e.keyCode == 13){
+                        e.target.value = "";
+                        this.handleButtonClick();
+                }
+        }
         render(){
                 return(
                         <div className="bottom-bar">
                                 <div className="message-send-wrapper input-group ">
                                         <div className="message-input-box-wrapper">
-                                                <input className="input-box form-control" type="text" value={this.state.currentMessageBoxValue} onChange={this.messageBoxOnChange}/>
+                                                <input className="input-box form-control" type="text" onKeyDown={this.keyPress} value={this.state.currentMessageBoxValue} onChange={this.messageBoxOnChange}/>
                                         </div>
 
                                         <div className="send-button-wrapper input-group-btn">
-                                                <button className="send-button btn btn-primary" onClick={this.handleButtonClick}>
+                                                <button className="send-button btn btn-primary"  onClick={this.handleButtonClick}>
                                                         Send           
                                                 </button>
                                         </div>
